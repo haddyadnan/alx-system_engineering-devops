e79e5fe2f2c828396ba3d6ae85c6add2cae098d7
haddyadnan <haddyadnan@gmail.com>
2022-09-18 14:19:22 +0100

    puppet test fileline

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,14 @@
 include stdlib
-file_line {'edit cmd':
+file_line {'edit Identity':
   ensure => present,
-  path   => '/tmp/school',
-  match  => '^IdentityFile',
-  line   => 'IdentityFile ~/.ssh/school'
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^IdentityFile'
+}
+
+file_line {'edit PasswordAuth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication'
 }
