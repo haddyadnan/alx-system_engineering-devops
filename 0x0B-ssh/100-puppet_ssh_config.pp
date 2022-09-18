ff261a30f53249a4e8f1898453f4cbe52c2b251d
haddyadnan <haddyadnan@gmail.com>
2022-09-18 14:08:22 +0100

    test fileline

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,7 @@
-file {'edit config':
-  ensure  => present,
-  path    => '/tmp/school',
-  content => 'Host *
-    		   SendEnv LANG LC_*
-    		   HashKnownHosts yes
-    		   GSSAPIAuthentication yes
-    		   PasswordAuthentication no
-    		   IdentityFile ~/.ssh/school
-
-		    '
+include stdlib
+file_line {'edit cmd':
+  ensure => present,
+  path   => '/tmp/school',
+  match  => '^IdentityFile',
+  line   => 'IdentityFile ~/.ssh/school'
 }
