5325afecd980fbae6c6b2825a7993377f904c441
haddyadnan <haddyadnan@gmail.com>
2022-09-21 01:12:15 +0100

    added more path for sed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Edit config file using puppet
 exec {'edit config':
   command => 'sed -i "s/PasswordAuthentication no/PasswordAuthentication yes" /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-  path    => ['/usr/bin', '/usr/sbin']
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
