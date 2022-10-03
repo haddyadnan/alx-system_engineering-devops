cce21e5fc269ff59dffcf8edd3019dce95b610c9
haddyadnan <haddyadnan@gmail.com>
2022-10-03 12:51:54 +0100

    create puppet manifest

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# Install nginx and Add http custom header with puppet
+exec {'apt-update':
+        command => '/usr/bin/apt-get -y update'
+}
+
+package {'install nginx':
+  ensure   => 'latest',
+  name     => 'nginx',
+
+}
+
+exec {'Add custom header':
+  command => 'sudo sed -i "17i\add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
+
+exec {'restart nginx':
+  command => 'sudo service nginx restart',
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
