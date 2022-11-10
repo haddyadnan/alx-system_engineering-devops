507169203baa5f507cee7f09031fa3262bf3ef68
haddyadnan <haddyadnan@gmail.com>
2022-11-10 16:18:24 +0100

    fix puppex-lint err

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #Increase u limit
 exec {'fix-for-nginx':
-   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+   command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+   provider => shell,
 }
